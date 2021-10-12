1a01aa594d53871522547f9e452a4d7da1de0de1
Eliott17 <poulaineliott@gmail.com>
2021-10-12 12:13:03 +0200

    history login finish

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Link, useHistory } from 'react-router-dom'
+import "./Login.css"
 
 import axios from "axios"
 
@@ -16,14 +17,13 @@ export default function Login() {
         }
         const loginResponse = await axios.post("http://localhost:8000/home/logIn", loginUser, { withCredentials: true }) //withCredentials => pour indiquer à Axios de passer le Cookie
             .then((res) => {
-                console.log(res);
-
-                // History Push à mettre ICI avec la condition
-                // if (cookie exist ?)
-                // if (res.message === "la phrase qu'il faut") {
-                //history.push("/home");
-                //} else { ici le cas ou c'est }
-
+                console.log(res.data.message);
+                if (res.data.message === "You are connected") {
+                    history.push("/home");
+                }
+                else {
+                    alert("Invalid email or password");
+                }
             })
             .catch((err) => {
                 console.log("coté front   ", err);
@@ -42,7 +42,7 @@ export default function Login() {
 
             </form>
             <Link to="/register">
-                <button>Créez votre compte</button>
+                <button className="create-account-button">Créez votre compte</button>
             </Link>
             <button className="forgot-password">Mot de passe oublié ?</button>
         </div>
